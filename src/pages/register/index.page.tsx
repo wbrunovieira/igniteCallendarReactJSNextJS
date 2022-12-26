f2cb4793212c9821547229c5b64ec8e4e4a171c4
wbrunovieira <wbrunovieira77@gmail.com>
2022-12-26 19:04:39 +0000

    added prisma, axios, setCookie and route create user

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,10 +1,12 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { AxiosError } from 'axios'
 import { useRouter } from 'next/router'
 import { ArrowRight } from 'phosphor-react'
 import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
+import { api } from '../../lib/axios'
 
 import { Container, Form, FormError, Header } from './styles'
 
@@ -41,8 +43,19 @@ export default function Register() {
     }
   }, [router.query?.username, setValue])
 
-  function handleRegister(data: RegisterFormData) {
-    console.log(data)
+  async function handleRegister(data: RegisterFormData) {
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err.response.data.message)
+        return
+      }
+      console.log(err)
+    }
   }
 
   return (
