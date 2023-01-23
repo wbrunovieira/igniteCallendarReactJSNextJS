f4d24db280849f5bc28d5dd452c3034880d1c321
wbrunovieira <wbrunovieira77@gmail.com>
2023-01-23 09:31:23 -0300

    navigation to the next month

--- src/pages/schedule/[username]/ScheduleForm/index.tsx
+++ src/pages/schedule/[username]/ScheduleForm/index.tsx
@@ -1,6 +1,7 @@
 // import { CalendarStep } from './CalendarStep'
-import { ConfirmStep } from './ConfirmStep'
+import { CalendarStep } from './CalendarStep'
+// import { ConfirmStep } from './ConfirmStep'
 
 export function ScheduleForm() {
-  return <ConfirmStep />
+  return <CalendarStep />
 }
